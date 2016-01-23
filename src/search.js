e46af90093f6e3725c854686d707747822a9c0fd
Boss2bmj <boss2bmj@gmail.com>
2016-01-23 12:21:12 +0700

    delete comment

--- src/search.js
+++ src/search.js
@@ -6,32 +6,13 @@ $(window).load(function() {
   gapi.client.load('youtube', 'v3', function() {
   $('#search-button').attr('disabled', false);
   });
-  $('#search').submit(function(event){
+  $('#search-button').submit(function(event){
     event.preventDefault();
     search();
   });
 });
 
-// Search for a specified string.
-function count(relo,q){
-  console.log("0999099990900990909009909");
-  if (relo > 0) {
-    console.log("77777777");
-    console.log(relo);
-    window.location.reload()
-    search(q);
-  }
-  relo++;
-  console.log(relo);
-  console.log("0999099990900990909009909");
-
-}
-
-
-var relo = 0;
-function search(q) {
-  // $( "#search-container" ).remove();
-  // var relo = 0;
+function search() {
   $( "#search-container" ).html("");
   var q = $('#query').val();
   console.log(q);
@@ -40,13 +21,6 @@ function search(q) {
     part: 'snippet',
     maxResults: 9
   });
-  // count(relo,q);
-  // console.log("0999099990900990909009909");
-
-
-  // console.log("0999099990900990909009909");
-
-
   request.execute(function(response) {
     console.log(response);
     var str = JSON.stringify(response.result);
@@ -55,33 +29,11 @@ function search(q) {
     var htmlvideo =""
     // console.log(str.items);
     // console.log(template);
-    // Mustache.parse(template);
-    console.log("------------");
     for (var i = 0; i < result.items.length; i++) {
       var item = result.items[i];
       var snippet = item.snippet;
-      console.log("************");
-      console.log("item");
-      console.log(item);
-
-      console.log("=======");
-      console.log("snippet");
-      console.log(snippet);
-      console.log("=======");
-      console.log("************");
-      // htmlvideo += Mustache.render(template, {id: item.id.videoId,title: snippet.title, imageurl: snippet.thumbnails.high.url});
       $('#search-container').append("<h2>"+snippet.title+"</h2>"+"<br>");
       $('#search-container').append("<iframe"+" "+"id='ytplayer'"+" "+"type ='text/html'"+" "+"class='embed-responsive-item'"+" "+"src='http://www.youtube.com/embed/"+item.id.videoId+"?autoplay=0'"+" "+"width='600'"+" "+"height='450'"+" "+"frameborder='0'/>"+"<br>");
-      // $('#search-container').append("<p>"+snippet.publishedAt+"</p>"+"<br>");
-      // $('#search-container').append("<p>"+snippet.description+"</p>"+"<br>");
-
-      // $('#search-container').append(<iframe id="ytplayer" type="text/html" class="embed-responsive-item" src="http://www.youtube.com/embed/"+item.id.videoId+"?autoplay=1" width="640" height="480" frameborder="0"+"<br>"/>);
-      // $('#search-container').append(item.id.videoId+" "+snippet.title+"<br>");
-
-
     }
   });
-
-
-
 }
