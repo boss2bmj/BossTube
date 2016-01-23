af0eefeca4326a240b7c92ed1a3baef32551ee4b
Boss2bmj <boss2bmj@gmail.com>
2016-01-23 17:48:16 +0700

    add enter press event

--- src/search.js
+++ src/search.js
@@ -4,18 +4,25 @@
 $(window).load(function() {
   gapi.client.setApiKey('AIzaSyDOSBjs7rY2BT2VbsaLoLg4pIVzkJo3cVo');
   gapi.client.load('youtube', 'v3', function() {
-  $('#search-button').attr('disabled', false);
+    $('#search-button').attr('disabled', false);
   });
   $('#search-button').submit(function(event){
     event.preventDefault();
     search();
   });
+  $('#query').keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+      event.preventDefault();
+        search();
+    }
+  });
 });
 
-function search() {
+function search(q) {
   $( "#search-container" ).html("");
   var q = $('#query').val();
-  console.log(q);
+  // console.log(q);
   var request = gapi.client.youtube.search.list({
     q: q,
     part: 'snippet',
@@ -27,9 +34,10 @@ function search() {
     // var template = $('#panelTemplate').html();
     var result = response.result;
     var htmlvideo =""
+    video_length = result.items.length;
     // console.log(str.items);
     // console.log(template);
-    for (var i = 0; i < result.items.length; i++) {
+    for (var i = 0; i < video_length; i++) {
       var item = result.items[i];
       var snippet = item.snippet;
       $('#search-container').append("<h2>"+snippet.title+"</h2>"+"<br>");
